77dbdd82ad2f14a9c0ab5253d6d7d93e0358ecf9
Christopher Bartling <chris.bartling@gmail.com>
2015-03-04 07:15:29 -0600

    Problem 3.

---
+++ problem-003/go/src/project-euler-003/project-euler-003.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 600851475143 
+	i := 2
+	for ; i * i < n; {
+		for ; (n % i) == 0; {
+			n = n / i
+		} 
+		i += 1
+	}
+	fmt.Printf("Solution: %d\n", n)
+}
+
+
